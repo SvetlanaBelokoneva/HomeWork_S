945651eb8b834abe03652199f1ffbdf9c13c7c92
SvetlanaBelokoneva <svet.vic@gmail.com>
2022-05-06 00:22:54 +0300

    HW_04 работа с объектами

---
+++ HomeWork_04/TestingSystem.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class TestingSystem {
+
+    private final static int QUESTION_COUNT = 3, ANSWER_COUNT = 5;
+    private static int correctCnt = 0, wrongCnt = 0;
+    private static int userOpinion;
+
+    public static void main(String[] args) {
+
+
+    }
+}
